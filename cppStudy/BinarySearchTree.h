f9baea9345a65f47d9856dbc6a30ea3fb18cfd9f
HeukKell <falcfmpcv@gmail.com>
2022-11-28 13:18:24 +0900

    Use enum to simplify code such as BST::insert function
    enum 을 사용하여 BST::insert 함수 등 코드 간결화
    
    # enum NodeType
     - BST Node 의 내부, 배열의 인덱스로 사용할 열거형

--- cppStudy/BinarySearchTree.h
+++ cppStudy/BinarySearchTree.h
@@ -1,5 +1,14 @@
 ﻿#pragma once
 
+/* BST Node 의 내부 노드 배열의 인덱스로 사용할 열거형 */
+enum NodeType {
+
+	PARENT,	// 0, 부모노드 인덱스
+	LCHILD,	// 1, 왼쪽 자식노드 인덱스
+	RCHILD,	// 2, 오른쪽 자식노드 인덱스
+	END,	// 3, 열거형의 개수, 노드의 개수, 및 null index 로 사용
+};
+
 
 /* data 부분에 해당하는 pair 구조체*/
 template <typename KeyType, typename ValueType>
@@ -13,22 +22,47 @@ struct Pair {
 	{
 
 	}
+
+	Pair(KeyType key, ValueType value)
+		: first(key)
+		, second(value)
+	{
+
+	}
 };
+
+/* 입력한 key 값과 value 값을 받아 , Pair 를 만들어 반환합니다.*/
+template<typename KeyType, typename ValueType>
+typename Pair<KeyType, ValueType> make_BSTPair(KeyType key, ValueType value)
+{
+	return Pair<KeyType, ValueType>(key, value);
+}
 
 
 template <typename KeyType, typename ValueType>
 struct BSTNode 
 {
 	Pair<KeyType,ValueType> pair;			// data
-	BSTNode* pParent;	// 부모노드
+	
+	/*
+	BSTNode* pParent;		// 부모노드
 	BSTNode* pLeftChild;	// 자식노드 왼쪽, 자신보다 작은값
 	BSTNode* pRightChild;	// 자식노드 오른쪽, 자신보다 큰값
+	*/ // 아래 배열로 바꾸어 선언
+
+	BSTNode* arrNode[(int)NodeType::END];			 // 이렇게 하면 3개노드 포인터가 선언된다. NodeType::END 는 type의 총 개수 3이 된다.
+	
 
 	BSTNode()
 		: pair()
-		, pParent(nullptr)
-		, pLeftChild(nullptr)
-		, pRightChild(nullptr)
+		, arrNode{}
+	{
+
+	}
+
+	BSTNode(const Pair<KeyType,ValueType>& _pair, BSTNode* pParentNode, BSTNode* pLeftChildNode, BSTNode* pRightChildNode)
+		: pair(_pair)
+		, arrNode{ pParentNode, pLeftChildNode, pRightChildNode }
 	{
 
 	}
@@ -64,9 +98,8 @@ public:
 template<typename KeyType, typename ValueType>
 inline bool BST<KeyType, ValueType>::insert(const Pair<KeyType,ValueType>& _pair)
 {
-	BSTNode<KeyType, ValueType>* pNewNode = new BSTNode<KeyType, ValueType>();
-	pNewNode->pair = _pair;	// 새로만든 노드에 데이터 부분은 복사를 통해 채워졌다.
-
+	BSTNode<KeyType, ValueType>* pNewNode = new BSTNode<KeyType, ValueType>(_pair, nullptr, nullptr, nullptr);
+	
 	// 첫번째 데이터라면
 	if (nullptr == pRootNode)
 	{
@@ -82,43 +115,35 @@ inline bool BST<KeyType, ValueType>::insert(const Pair<KeyType,ValueType>& _pair
 		*/
 		
 		BSTNode<KeyType, ValueType>* pNode = pRootNode; // 루트노드를 가리키고
-		
+		enum NodeType  nodeType = NodeType::END;		// 아무것도 가리키지 않는 타입으로 선언, 이것을 BSTNode 의 인덱스로 넣어줄것이야. PARENT = 0, LCHILD = 1, RCHILD =2, END = 3
+
 		// 자리 찾아서 연결
 		while (true)
 		{
-			if (pNode->pair.first > pNewNode->pair.first)
-			{
-				if (nullptr == pNode->pLeftChild)
-				{
-					pNode->pLeftChild = pNewNode;
-					pNewNode->pParent = pNode;
-					++iCount;
-					break;
-				}
-				else {
-					pNode = pNode->pLeftChild;
-				}
+			// 신규노드의 키값이 루트노드보다 작다면, 왼쪽자식 으로 가야지
+			if (pNode->pair.first > pNewNode->pair.first){
+				nodeType = NodeType::LCHILD;
 			}
-			else if (pNode->pair.first < pNewNode->pair.first)
-			{
-				if (nullptr == pNode->pRightChild)
-				{
-					pNode->pRightChild = pNewNode;
-					pNewNode->pParent = pNode;
-					++iCount;
-					break;
-				}
-				else {
-					pNode = pNode->pRightChild;
-				}
+			else if (pNode->pair.first < pNewNode->pair.first){
+				nodeType = NodeType::RCHILD;
 			}
-			else {
-				/* 값이 같을경우..
-				* 중복되는 key 값을 허용하는 map 이 따로 존재합니다. multi map
-				* map 의 경우 중복되는 key 값이 있으면 안넣기 때문에 미리 체크하는것이 중요하다.
-				*/ 
+			else { // key 값이 같다면, 중복 key 값은 허용하지 않을것이야. insert 실패 -> false 로 반환
 				return false;
 			}
+
+			// 가고자 하는 pNode 의 자식이 없을때까지 반복할것이야. 자식이 nullptr 이 아니라면 거기에 삽입하면 되겠지.
+			if (nullptr == pNode->arrNode[(int)nodeType]) // 왼쪽 자식이든 오른쪽 자식이든 
+			{
+				pNode->arrNode[(int)nodeType] = pNewNode; // 자식노드 설정
+				pNewNode->arrNode[(int)NodeType::PARENT] = pNode;  // 부모 노드 설정
+				++iCount;
+				break;
+			}
+			else {
+				// 가고자 하는 쪽에 자식이 이미 있는경우
+				pNode = pNode->arrNode[(int)nodeType];
+			}
+
 		}
 		
 		
